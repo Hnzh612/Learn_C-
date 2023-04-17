1f0934ae8c65ba23aacedca2c704987abc518dde
HnZh <15120376892@163.com>
2023-04-17 16:03:35 +0800

    用json序列化参数

---
+++ JSON_prictices/JSON_prictices/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace JSON_prictices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLStepMap obj = new XMLStepMap();
+
+
+            string _dirPath = $"{Environment.CurrentDirectory}/Params";
+            if(!Directory.Exists(_dirPath))
+            {
+                Directory.CreateDirectory(_dirPath);
+            }
+            string FilePath = $"{_dirPath}/params.json";
+            if(!File.Exists(_dirPath))
+            {
+                File.Create(FilePath).Close();
+            }
+            
+            using(StreamWriter file = new StreamWriter(FilePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, obj);
+            }
+        }
+    }
+    public class MyObj
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public List<string> Games { get; set; }
+    }
+}
---
+++ JSON_prictices/JSON_prictices/StepLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_prictices
+{
+    public struct StepLine
+    {
+        public StepLine(XYZDPoint _x, XYZDPoint _y)
+        {
+            Start = _x;
+            End = _y;
+        }
+        public void SetStart(XYZDPoint _x)
+        {
+            Start = _x;
+        }
+        public void SetEnd(XYZDPoint _y)
+        {
+            End = _y;
+        }
+        /// <summary>
+        /// 开始坐标
+        /// </summary>
+        public XYZDPoint Start { get; set; }
+        /// <summary>
+        /// 结尾坐标
+        /// </summary>
+        public XYZDPoint End { get; set; }
+    }
+}
