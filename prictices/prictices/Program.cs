c35849f7fb8865ac96b2888f5dc46b39a96c1563
HnZh <15120376892@163.com>
2023-05-11 16:40:52 +0800

    联系

--- prictices/prictices/Program.cs
+++ prictices/prictices/Program.cs
@@ -1,12 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] ar;
-ar = new int[] { 1, 2, 3 };
+using prictices;
 
-string name = "H,n,z,h,h";
+double a = 28600000;
+double b = 472;
+double c = 1060;
+double sum = a+b+c;
 
-string[] strArray = name.Split(",");
-foreach (string str in strArray)
-{
-    Console.Write(str+ " ");
-}
+Dictionary<string,double> map = new Dictionary<string,double>();
+map.Add("HPV16", a);
+map.Add("HPV18", b);
+map.Add("HPV32", c);
+Capacity.VirusMap = map;
+Capacity.allExpotend = sum;
+Dictionary<string,double> capacity =  Capacity.CalPercentage();
+Console.WriteLine(capacity["HPV16"].ToString("P"));
+Console.WriteLine(capacity["HPV18"].ToString("P"));
+Console.WriteLine(capacity["HPV32"].ToString("P"));
+
